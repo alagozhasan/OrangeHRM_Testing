d0510bfd3bfe3c8e73d7d9c580f28716954c2dd9
resulhurfikir <resulhurfikir@gmail.com>
2023-04-05 02:33:02 +0300

    asdaka

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -1,5 +1,18 @@
 package Pages;
 
-public class DialogContent {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class DialogContent {
+    @FindBy(css="button[aria-label='LOGIN']")
+    public WebElement loginButton;
+
+    @FindBy(css="span[class='mat-tooltip-trigger logo-text']")
+    public WebElement txtTechnoStudy;
+
+    @FindBy(xpath="//ms-add-button[contains(@tooltip,'ADD')]//button")
+    public WebElement addButton;
+
+    @FindBy(xpath="//ms-text-field[@formcontrolname='name']/input")
+    public WebElement nameInput;
 }
