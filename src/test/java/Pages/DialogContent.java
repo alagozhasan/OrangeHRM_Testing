9c01dc40ac784f04c3d2d551a693bdb9a89643f7
Hasan ALAGOZ <123511537+alagozhasan@users.noreply.github.com>
2023-04-05 02:33:34 +0300

    asd

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -1,5 +1,20 @@
 package Pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class DialogContent {
+
+    @FindBy(css = "input[formcontrolname='username']")
+    public WebElement username;
+
+    @FindBy(css="input[formcontrolname='password']")
+    public WebElement password;
+
+    @FindBy(css="button[aria-label='LOGIN']")
+    public WebElement loginButton;
+
+    @FindBy(css="span[class='mat-tooltip-trigger logo-text']")
+    public WebElement txtTechnoStudy;
 
 }
