53dd885206f4a8275db35279a1965786e5d77569
Hasan ALAGOZ <123511537+alagozhasan@users.noreply.github.com>
2023-04-05 22:24:46 +0300

    nameinput passwordinput

---
+++ src/test/java/Pages/DialogContent.java
@@ -0,0 +1,19 @@
+package Pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DialogContent {
+    public static void main(String[] args) {
+        System.out.println("Factory");
+
+    }
+    @FindBy(css = "")
+    public WebElement nameInput;
+
+    @FindBy(css = "")
+    public WebElement passwordInput;
+
+
+
+}
