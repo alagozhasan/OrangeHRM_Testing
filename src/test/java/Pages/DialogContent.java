03a8b9a5b56bef8c605ba625c3daa3a787786d37
burcucokcan <cokcanburcu@gmail.com>
2023-04-05 22:45:53 +0300

    burcu commmit2

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.support.FindBy;
 public class DialogContent {
     public static void main(String[] args) {
         System.out.println("Factory");
-
+        System.out.println("Burcuuu");
+        
     }
     @FindBy(css = "")
     public WebElement nameInput;
@@ -19,6 +20,30 @@ public class DialogContent {
 //    @FindBy(css="input[formcontrolname='password']")
 //    public WebElement password; // Aynısından vardı
 
+    @FindBy(css="button[aria-label='LOGIN']")
+    public WebElement loginButton;
+
+    @FindBy(css="span[class='mat-tooltip-trigger logo-text']")
+    public WebElement txtTechnoStudy;
+
+    @FindBy(xpath="//ms-add-button[contains(@tooltip,'ADD')]//button")
+    public WebElement addButton;
+
+
+
+    @FindBy(xpath="//ms-save-button/button")
+    public WebElement saveButton;
+
+    @FindBy(xpath="//div[contains(text(),'successfully')]")
+    public WebElement successMessage;
+
+    @FindBy(xpath="//ms-search-button//button")
+    public WebElement searchButton;
+
+    @FindBy(xpath="//div[contains(text(),'already exists')]")
+    public WebElement alreadyExist;
+    @FindBy(xpath="//ms-text-field[@formcontrolname='shortName']/input")
+    public WebElement shortName;
 
 
 
