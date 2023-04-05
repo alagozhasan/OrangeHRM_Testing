740aba0ff0a9a35728ef21a2a2f322b7e1c9829d
resulhurfikir <resulhurfikir@gmail.com>
2023-04-06 01:29:16 +0300

    Proje başlangıcı yapıldı. Birçok arkadaşta olacak genel metodlar oluşturuldu ve devam ediliyor.

--- src/test/java/FeatureFiles/TC01_Resul.feature
+++ src/test/java/FeatureFiles/TC01_Resul.feature
@@ -1,1 +1,12 @@
-Feature:
+
+  Feature:  ESS User Functionalitiy
+
+    Scenario: Creatation an ESS user
+
+      Given Navigate to OrangeHRM Website
+      When I login as an admin
+      And I navigate to user management page
+      When I click on add button
+      And I fill up the form with required info
+      And I click on save button
+      Then I verify that ESS user on the list
---
+++ src/test/java/StepDefinitions/TC01_ResulSteps.java
@@ -0,0 +1,37 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TC01_ResulSteps {
+    @Given("Navigate to OrangeHRM Website")
+    public void navigateToOrangeHRMWebsite() {
+
+    }
+
+    @When("I login as an admin")
+    public void iLoginAsAnAdmin() {
+    }
+
+    @And("I navigate to user management page")
+    public void iNavigateToUserManagementPage() {
+    }
+
+    @When("I click on add button")
+    public void iClickOnAddButton() {
+    }
+
+    @And("I fill up the form with required info")
+    public void iFillUpTheFormWithRequiredInfo() {
+    }
+
+    @And("I click on save button")
+    public void iClickOnSaveButton() {
+    }
+
+    @Then("I verify that ESS user on the list")
+    public void iVerifyThatESSUserOnTheList() {
+    }
+}
