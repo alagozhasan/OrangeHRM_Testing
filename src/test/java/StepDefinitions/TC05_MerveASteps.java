f4c74e8a82e991c80f501b0782894bac75376173
merve <mervegz263@gmail.com>
2023-04-07 02:55:55 +0300

    Merve A case 5 düzeltme methodlar

--- src/test/java/StepDefinitions/TC05_MerveASteps.java
+++ src/test/java/StepDefinitions/TC05_MerveASteps.java
@@ -7,27 +7,6 @@ import io.cucumber.java.en.*;
 
 public class TC05_MerveASteps {
     Elements e = new Elements();
-    @Given("Navigate to OrangeHRM Website")
-    public void navigateToOrangeHRMWebsite() {
-        GWD.getDriver().get("https://opensource-demo.orangehrmlive.com/");
-    }
-
-    @When("I login as an admin")
-    public void iLoginAsAnAdmin() {
-        e.sendKeysFunction(e.loginUsername,"Admin");
-        e.sendKeysFunction(e.loginPassword,"admin123");
-        e.clickFunction(e.loginButton);
-    }
-
-    @And("I navigate to admin management page")
-    public void iNavigateToUserManagementPage() {
-        e.clickFunction(e.adminButton);
-    }
-
-    @When("I click on add button")
-    public void iClickOnAddButton() {
-        e.javaScpritClickFunction(e.addButton);
-    }
 
     @And("I enter an invalid name in to the Employee name field")
     public void iEnterAnInvalidNameInToTheEmployeeNameField() {
