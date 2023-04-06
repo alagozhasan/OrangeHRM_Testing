2dc94dbd1ecc38b8ac63bd6dd554455b3970ede5
merve <mervegz263@gmail.com>
2023-04-07 02:10:01 +0300

    Merve A case 5 düzeltme yapıldı

--- src/test/java/FeatureFiles/TC05_MerveA.feature
+++ src/test/java/FeatureFiles/TC05_MerveA.feature
@@ -3,7 +3,8 @@ Feature:Employee name field
   Scenario:Verifying admin name in Employee Field
 
     Given Navigate to OrangeHRM Website
-    When  Navigate to admin management page
-    And   I click on add button
+    When  I login as an admin
+    And   I navigate to admin management page
+    When  I click on add button
     And   I enter an invalid name in to the Employee name field
     Then  I verify that notification messages “no records found” and “invalid” in red color displayed.
--- src/test/java/StepDefinitions/TC05_MerveASteps.java
+++ src/test/java/StepDefinitions/TC05_MerveASteps.java
@@ -2,10 +2,8 @@ package StepDefinitions;
 
 import Pages.Elements;
 import Utilities.GWD;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.en.*;
+
 
 public class TC05_MerveASteps {
     Elements e = new Elements();
@@ -32,13 +30,13 @@ public class TC05_MerveASteps {
     }
 
     @And("I enter an invalid name in to the Employee name field")
-    public void ıEnterAnInvalidNameInToTheEmployeeNameField() {
+    public void iEnterAnInvalidNameInToTheEmployeeNameField() {
         e.sendKeysFunction(e.employeeName,"Merve");
 
     }
 
     @Then("I verify that notification messages “no records found” and “invalid” in red color displayed.")
-    public void ıVerifyThatNotificationMessagesNoRecordsFoundAndInvalidInRedColorDisplayed() {
+    public void iVerifyThatNotificationMessagesNoRecordsFoundAndInvalidInRedColorDisplayed() {
         e.displaymessage.isDisplayed();
     }
 }
