7ab0f311ce8c14bb7c6068efc9daa25e08523aea
burcucokcan <cokcanburcu@gmail.com>
2023-04-07 07:35:44 +0300

    Burcu TestCase03 elements, feature file ve steps are added

--- src/test/java/Pages/Parent.java
+++ src/test/java/Pages/Parent.java
@@ -1,18 +1,13 @@
 package Pages;
 
 import Utilities.GWD;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Parent {
